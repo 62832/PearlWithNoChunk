aeeaf8851a55b9890c227b0df835f2bfb0415968
90 <hi@90.gripe>
2025-01-29 10:30:59 +0000

    Port to Fabric

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,37 @@
 plugins {
-    id("net.neoforged.moddev")
+    id("fabric-loom")
 }
 
-base.archivesName = "pearlwithnochunk"
+base.archivesName = "pearlwithnochunk-fabric"
 version = "0.1"
 group = "gripe.90"
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(21)
 
-neoForge {
-    version = "21.3.48-beta"
-
-    parchment {
-        minecraftVersion = "1.21"
-        mappingsVersion = "2024.11.10"
-    }
-
+loom {
     mods {
         create("pearlwithnochunk") {
             sourceSet(sourceSets.main.get())
         }
     }
+}
 
-    runs {
-        create("client") {
-            client()
-            gameDirectory = file("run")
-        }
+repositories {
+    maven {
+        name = "ParchmentMC"
+        url = uri("https://maven.parchmentmc.org")
     }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:1.21.3")
+    modImplementation("net.fabricmc:fabric-loader:0.16.10")
+
+    @Suppress("UnstableApiUsage")
+    mappings(loom.layered {
+        officialMojangMappings()
+        parchment("org.parchmentmc.data:parchment-1.21.3:2024.12.07@zip")
+    })
 }
 
 tasks {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,19 @@
 pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
     plugins {
-        id("net.neoforged.moddev") version "2.0.49-beta"
-        id("net.neoforged.moddev.repositories") version "2.0.49-beta"
+        id("fabric-loom") version "1.9-SNAPSHOT"
     }
 }
 
 plugins {
-    id("net.neoforged.moddev.repositories")
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
